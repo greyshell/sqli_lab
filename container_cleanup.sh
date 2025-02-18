7611fe69bfc534a38194ebc061960bf8cff0f4fe
ABHIJIT SINHA <grey.shell@gmail.com>
2025-02-18 05:10:44 +0000

    refresh the challenges

---
+++ container_cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# author: greyshell
+
+sudo docker stop $(sudo docker ps -a -q);
+sudo docker rm $(sudo docker ps -a -q);
+sudo docker volume rm $(sudo docker volume ls -f dangling=true -q);
+sudo docker rmi -f $(sudo docker images -a -q);
+sudo docker system prune -a --volumes -f
+
