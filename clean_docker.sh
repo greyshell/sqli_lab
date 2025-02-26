744c3972801481e6cc1998f9ce54500909164f38
ABHIJIT SINHA <grey.shell@gmail.com>
2025-02-26 23:47:06 +0000

    updated

--- clean_docker.sh
+++ clean_docker.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # author: greyshell
 
-sudo docker-compose down
 sudo docker stop $(sudo docker ps -a -q);
 sudo docker rm $(sudo docker ps -a -q);
 sudo docker volume rm $(sudo docker volume ls -f dangling=true -q);
